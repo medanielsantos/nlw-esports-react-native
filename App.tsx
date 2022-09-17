dfd08e7ee19a5fe1b02f0fb96ce5b4e2caa69878
Daniel Henrique <medanielsantos@gmail.com>
2022-09-17 18:54:20 -0300

    add custum font

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" backgroundColor="red"/>
-    </View>
+    <Background>
+      <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+      
+
+    </Background>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-});
