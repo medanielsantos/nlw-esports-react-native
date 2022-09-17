4ef1149a1408dd2eb368f4ae9aac7285c57c5382
Daniel Henrique <medanielsantos@gmail.com>
2022-09-17 19:38:12 -0300

    create component heading

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,7 @@ import { View, Image } from 'react-native';
 import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 
 export function Home() {
@@ -13,6 +14,11 @@ export function Home() {
       <Image 
         source={ logoImg } 
         style={ styles.logo } />
+
+        <Heading 
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+        />
 
     </View>
   );
